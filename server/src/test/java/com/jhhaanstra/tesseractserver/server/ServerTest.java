a5b734696e1eb195bde9236faf24c5170af5625e
Jan Hendrik Haanstra <janhendrikhaanstra@gmail.com>
2023-03-12 11:21:46 +0100

    Created a base class for server tests

--- server/src/test/java/com/jhhaanstra/tesseractserver/server/ServerTest.java
+++ server/src/test/java/com/jhhaanstra/tesseractserver/server/ServerTest.java
@@ -1,23 +1,13 @@
 package com.jhhaanstra.tesseractserver.server;
 
-import com.jhhaanstra.tesseractserver.server.ocr.OcrController;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-class ServerTest {
-
-    @Autowired
-    private OcrController controller;
+class ServerTest extends ApplicationTestBase {
 
     @Test
-    void shouldBootServer() {
-        assertThat(controller).isNotNull();
+    void shouldGetStatus() {
+        AssertionsForClassTypes.assertThat(restTemplate.getForObject(getEndpoint("/status"), String.class))
+                .contains("running");
     }
 }
